d0c8a4ef60f1e44b178d7f90ccb8a7e12b746493
poor <christian.brinkmann@uni-weimar.de>
2019-08-21 21:32:21 +0200

    initial implementation of substitution feature

--- blender-viewport-rename.py
+++ blender-viewport-rename.py
@@ -23,7 +23,7 @@ import re
 bl_info = {
     "name": "Viewport Rename",
     "author": "Christian Brinkmann (p2or)",
-    "version": (0, 5),
+    "version": (0, 6),
     "blender" : (2, 80, 0),
     "location": "3D View > Ctrl+R",
     "warning": "", # used for warning icon and text in addons panel
@@ -33,7 +33,7 @@ bl_info = {
 }
 
 
-class ViewportRenameOperator(bpy.types.Operator):
+class VIEW3D_OT_viewport_rename(bpy.types.Operator):
     """Rename selected object(s) in 3D View"""
     bl_idname = "view3d.viewport_rename"
     bl_label = "Viewport Rename"
@@ -41,45 +41,102 @@ class ViewportRenameOperator(bpy.types.Operator):
     bl_property = "new_name"
 
     new_name : bpy.props.StringProperty(name="New Name")
+    substitute : bpy.props.StringProperty(name="Replace")
     data_flag : bpy.props.BoolProperty(name="Rename Data-Block", default=False)
+    mode : bpy.props.EnumProperty(name="Mode", description="Set the Mode",
+        items = [('RENAME', "Rename", ""),
+                ('RESEARCH', "Search & Replace", ""),
+                ('SEARCH', "Search & Select", "")])
 
     @classmethod
     def poll(cls, context):
-        return bool(context.selected_objects)
+        return bool(context.active_object) #selected_objects
 
     def execute(self, context):
         user_input = self.new_name
-        reverse = False
-        if user_input.endswith("#r"):
-            reverse = True
-            user_input = user_input[:-1]
-
-        suff = re.findall("#+$", user_input)
-        if user_input and suff:
-            number = ('%0'+str(len(suff[0]))+'d', len(suff[0]))
-            real_name = re.sub("#", '', user_input)           
-
-            objs = context.selected_objects[::-1] if reverse else context.selected_objects
-            names_before = [n.name for n in objs]
-            for c, o in enumerate(objs, start=1):
-                o.name = (real_name + (number[0] % c))
-                if self.data_flag and o.data is not None:
-                    o.data.name = (real_name + (number[0] % c))
-            self.report({'INFO'}, "Renamed {}".format(", ".join(names_before)))
-            return {'FINISHED'}
-
-        elif user_input:
-            old_name = context.active_object.name
-            context.active_object.name = user_input
-            if self.data_flag and context.active_object.data is not None:
-                context.active_object.data.name = user_input
-            self.report({'INFO'}, "{} renamed to {}".format(old_name, user_input))
-            return {'FINISHED'}
-
-        else:
+        
+        if not user_input:
             self.report({'INFO'}, "No input, operation cancelled")
             return {'CANCELLED'}
-    
+        
+        # -------------------------------------------------------
+        
+        if self.mode == 'RENAME':
+            reverse = False
+            if user_input.endswith("#r"):
+                reverse = True
+                user_input = user_input[:-1]
+
+            suff = re.findall("#+$", user_input)
+            if user_input and suff:
+                number = ('%0'+str(len(suff[0]))+'d', len(suff[0]))
+                real_name = re.sub("#", '', user_input)           
+
+                objs = context.selected_objects[::-1] if reverse else context.selected_objects
+                names_before = [n.name for n in objs]
+                for c, o in enumerate(objs, start=1):
+                    o.name = (real_name + (number[0] % c))
+                    if self.data_flag and o.data is not None:
+                        o.data.name = (real_name + (number[0] % c))
+                self.report({'INFO'}, "Renamed {}".format(", ".join(names_before)))
+                return {'FINISHED'}
+
+            elif user_input:
+                old_name = context.active_object.name
+                context.active_object.name = user_input
+                if self.data_flag and context.active_object.data is not None:
+                    context.active_object.data.name = user_input
+                self.report({'INFO'}, "{} renamed to {}".format(old_name, user_input))
+                return {'FINISHED'}
+        
+        # -------------------------------------------------------
+        
+        elif self.mode == 'SEARCH':
+            candidates = []
+            if self.data_flag:
+                for obj in context.scene.objects:
+                    if obj.data is not None and self.new_name in obj.data.name:
+                        candidates.append(obj)
+            else:
+                for obj in context.scene.objects:
+                    if self.new_name in obj.name:
+                        candidates.append(obj)
+
+            if candidates:
+                bpy.ops.object.select_all(action='DESELECT')
+                for obj in candidates:
+                    # limited by current API state,
+                    # should be replaced with visible_set(True) when available
+                    if obj.visible_get():
+                        obj.select_set(True)
+                self.report({'INFO'}, "Found {} objects".format(len(candidates)))
+                return {'FINISHED'}
+
+            else:
+                bpy.ops.object.select_all(action='DESELECT')
+                self.report({'INFO'}, "Nothing found.")
+                return {'CANCELLED'}
+
+        # -------------------------------------------------------
+        
+        elif self.mode == 'RESEARCH':
+            candidates = []
+            for obj in context.selected_objects:
+                if self.new_name in obj.name:
+                    candidates.append(obj)
+
+            if candidates:
+                for obj in candidates:
+                    obj.name = obj.name.replace(self.new_name, self.substitute)
+                    if self.data_flag and obj.data is not None:
+                        obj.data.name = obj.name
+                self.report({'INFO'}, "Renamed {} objects".format(len(candidates)))
+                return {'FINISHED'}
+            else:
+                self.report({'INFO'}, "Nothing found.")
+                return {'CANCELLED'}
+
+
     def invoke(self, context, event):
         wm = context.window_manager
         dpi = context.preferences.system.pixel_size
@@ -90,15 +147,25 @@ class ViewportRenameOperator(bpy.types.Operator):
         return wm.invoke_props_dialog(self, width=dialog_size)
 
     def draw(self, context):
-        row = self.layout
-        row.row()
-        row.prop(self, "new_name")
-        row.prop(self, "data_flag")
-        row.row()
+        txt_name = "New Name" if self.mode == "RENAME" else "Search for"
+        txt_data = "Rename Data-Block" if self.mode != "SEARCH" else "Find Data-Block"
+
+        layout = self.layout
+        layout.row()
+        layout.prop(self, "mode", expand=True)
+        layout.row()
+        layout.prop(self, "new_name", text=txt_name)
+
+        if self.mode == "RESEARCH":
+            rep = layout.row()
+            rep.prop(self, "substitute", text="Replace with")
+
+        layout.prop(self, "data_flag", text=txt_data)
+        layout.row()
 
 
 # ------------------------------------------------------------------------
-#    register, unregister and hotkey
+#    register, unregister and hotkeys
 # ------------------------------------------------------------------------
 
 addon_keymaps = []
@@ -107,14 +174,14 @@ def register():
     from bpy.utils import register_class
 
     addon_keymaps.clear()
-    register_class(ViewportRenameOperator)
+    register_class(VIEW3D_OT_viewport_rename)
 
     # handle the keymap
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
     if kc:
         km = wm.keyconfigs.addon.keymaps.new(name='3D View', space_type='VIEW_3D')
-        kmi = km.keymap_items.new(ViewportRenameOperator.bl_idname, type='R', value='PRESS', ctrl=True)
+        kmi = km.keymap_items.new(VIEW3D_OT_viewport_rename.bl_idname, type='R', value='PRESS', ctrl=True)
         addon_keymaps.append((km, kmi))
 
 def unregister():
@@ -124,7 +191,7 @@ def unregister():
         km.keymap_items.remove(kmi)
     addon_keymaps.clear()
 
-    unregister_class(ViewportRenameOperator)
+    unregister_class(VIEW3D_OT_viewport_rename)
 
 if __name__ == "__main__":
     register()
